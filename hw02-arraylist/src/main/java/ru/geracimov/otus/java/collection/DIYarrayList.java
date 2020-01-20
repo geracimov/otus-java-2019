68343d61f50bccd49cc5c951564296dc78634bd3
geracimov <geracimov@gmail.com>
2020-01-20 22:31:19 +0300

    [DIYarrayList] added some method's implementations. Updated tests

--- hw02-arraylist/src/main/java/ru/geracimov/otus/java/collection/DIYarrayList.java
+++ hw02-arraylist/src/main/java/ru/geracimov/otus/java/collection/DIYarrayList.java
@@ -4,18 +4,25 @@ import java.util.*;
 import java.util.function.UnaryOperator;
 
 public final class DIYarrayList<T> implements List<T> {
-    private final static Object[] DEFAULT_ELEMENTS = {};
+    private final static float DEFAULT_FACTOR = 1.5f;
+    private static final int DEFAULT_CAPACITY = 8;
 
     private transient Object[] elements;
     private int size;
+    private float factor;
 
     public DIYarrayList() {
-        this.elements = DEFAULT_ELEMENTS;
+        this(DEFAULT_CAPACITY, DEFAULT_FACTOR);
     }
 
     public DIYarrayList(int size) {
+        this(size, DEFAULT_FACTOR);
+    }
+
+    public DIYarrayList(int size, float increaseFactor) {
         if (size > 0) {
             this.elements = new Object[size];
+            this.factor = increaseFactor;
         } else {
             throw new IllegalArgumentException("Size must be positive");
         }
@@ -36,8 +43,12 @@ public final class DIYarrayList<T> implements List<T> {
         if (index > size || index < 0) {
             throw new IndexOutOfBoundsException(String.format("Index: %d, Size: %d", index, size));
         }
-
-
+        if (elements.length == size) {
+            increase(factor);
+        }
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = element;
+        size++;
     }
 
     @Override
@@ -47,73 +58,94 @@ public final class DIYarrayList<T> implements List<T> {
     }
 
     @Override
-    public boolean contains(Object o) {
-        return false;
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-        return null;
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public <T1> T1[] toArray(T1[] a) {
-        return null;
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
     public boolean addAll(Collection<? extends T> c) {
-        return false;
+        for (T element : c) {
+            this.add(element);
+        }
+        return true;
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends T> c) {
-        return false;
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return indexOf(o) >= 0;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(elements, size);
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public T remove(int index) {
+        final Object element = elements[index];
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        elements[size - 1] = null;
+        size--;
+        return (T) element;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        int index = indexOf(o);
+        if (index < 0) return false;
+        remove(index);
+        return true;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void replaceAll(UnaryOperator<T> operator) {
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void sort(Comparator<? super T> c) {
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void clear() {
-
+        elements = new Object[]{};
+        size = 0;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index > size - 1) {
+            throw new IndexOutOfBoundsException(String.format("Index: %d, Size: %d", index, size));
+        }
+        return (T) elements[index];
     }
 
     @Override
@@ -123,37 +155,76 @@ public final class DIYarrayList<T> implements List<T> {
 
 
     @Override
-    public T remove(int index) {
-        return null;
-    }
-
-    @Override
     public int indexOf(Object o) {
-        return 0;
+        if (o == null) {
+            throw new IllegalArgumentException("Element cannot be null");
+        }
+        for (int i = 0; i < size; i++) {
+            if (o.equals(elements[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        if (o == null) {
+            throw new IllegalArgumentException("Element cannot be null");
+        }
+        for (int i = size - 1; i >= 0; i--) {
+            if (o.equals(elements[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public ListIterator<T> listIterator() {
-        return null;
+        throw new UnsupportedOperationException();
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public ListIterator<T> listIterator(int index) {
-        return null;
-    }
-
-    @Override
-    public List<T> subList(int fromIndex, int toIndex) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Spliterator<T> spliterator() {
-        return null;
+        throw new UnsupportedOperationException();
     }
+
+    @Override
+    public String toString() {
+        if (size == 0) {
+            return "DIYarrayList{}";
+        }
+        StringBuilder sb = new StringBuilder("DIYarrayList{");
+        for (int i = 0; i < size; i++) {
+            sb.append(elements[i]);
+            if (i != size - 1) {
+                sb.append(", ");
+            }
+        }
+        return sb.append("}").toString();
+    }
+
+    private void increase(float factor) {
+        if (factor <= 1.0) {
+            throw new IllegalArgumentException(String.format("Factor %f must be greater then 1.0", factor));
+        }
+        final float rawSize = Math.max(elements.length, DEFAULT_CAPACITY) * factor;
+        int newSize = rawSize > Integer.MAX_VALUE ? Integer.MAX_VALUE : Math.round(rawSize);
+        Object[] newElements = new Object[newSize];
+        System.arraycopy(elements, 0, newElements, 0, elements.length);
+        elements = newElements;
+    }
+
 }
--- hw02-arraylist/src/test/java/ru/geracimov/otus/java/collection/DIYarrayListTest.java
+++ hw02-arraylist/src/test/java/ru/geracimov/otus/java/collection/DIYarrayListTest.java
@@ -1,9 +1,11 @@
 package ru.geracimov.otus.java.collection;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -11,27 +13,39 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 @DisplayName("Реализация DIYarrayList")
 class DIYarrayListTest {
 
+    private DIYarrayList<Long> longList;
+
+    @BeforeEach
+    void init() {
+        longList = new DIYarrayList<>();
+        for (long l = 0L; l < 100L; l++) {
+            longList.add(l);
+        }
+    }
 
     @Test
     @DisplayName("Корректно возвращает размер пустого списка")
     void emptyListSizeTest() {
         DIYarrayList<String> strings = new DIYarrayList<>();
+        assertThat(strings).isNotNull();
         assertThat(strings.size()).isEqualTo(0);
-        assertThat(strings.isEmpty()).isEqualTo(true);
     }
 
     @Test
     @DisplayName("Бросает исключение при инициализации списка с отрицательным размером")
     void initListSizeNegativeSizeTest() {
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            DIYarrayList<String> strings = new DIYarrayList<>(-1);
-        }).withMessageMatching("Size must be positive");
+        assertThatIllegalArgumentException().isThrownBy(() -> new DIYarrayList<>(-1))
+                .withMessageMatching("Size must be positive");
     }
 
     @Test
     @DisplayName("Корректно возвращает признак пустого списка")
     void emptyListIsEmptyTest() {
         DIYarrayList<String> strings = new DIYarrayList<>();
+        //noinspection ConstantConditions
+        assertThat(strings.isEmpty()).isEqualTo(true);
+        strings.add("something");
+        assertThat(strings.isEmpty()).isEqualTo(false);
     }
 
     @Test
@@ -39,15 +53,39 @@ class DIYarrayListTest {
     void emptyInitListSizeTest() {
         DIYarrayList<String> strings = new DIYarrayList<>(10);
         assertThat(strings.size()).isEqualTo(0);
+        strings.add("something1");
+        strings.add("something2");
+        assertThat(strings.size()).isEqualTo(2);
     }
 
+    @Disabled
     @Test
-    void name() {
-        int[] src = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int[] dst = new int[src.length * 2];
-        System.out.println(Arrays.toString(Arrays.copyOf(src, 4)));
-        System.arraycopy(src, 0, dst, 0, src.length);
-        dst[src.length] = 99;
-        System.out.println(Arrays.toString(dst));
+    @DisplayName("Корректно добавляет элементы в коллекцию")
+    void addElementInListTest() {
+        longList.add(1, 0L);
+        longList.add(1, -1L);
+        assertThat(longList).containsSequence(0L, 1L, -1L, 0L, 2L, 3L);
+        assertThat(longList).hasSize(5);
     }
+
+    @Disabled
+    @Test
+    @DisplayName("Корректно добавляет несколько элементов в коллекцию")
+    void addAllElementInListTest() {
+        Collections.addAll(longList, 100L, 101L, 102L, 103L);
+        assertThat(longList).containsSequence(98L, 99L, 100L, 101L, 102L, 103L);
+        assertThat(longList).hasSize(104);
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("Корректно удаляет элементы из коллекции")
+    void dddd() {
+        longList.remove(6L);
+        longList.remove(7L);
+        longList.remove(54L);
+        assertThat(longList).hasSize(97);
+        assertThat(longList).doesNotContain(6L, 7L, 54L);
+    }
+
 }
