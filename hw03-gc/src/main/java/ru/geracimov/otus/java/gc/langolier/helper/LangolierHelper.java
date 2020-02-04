10487973c8f669708256ee15e87a0410d98a65fa
geracimov <geracimov@gmail.com>
2020-02-04 22:02:03 +0300

    [hw03-gc] Added: * sleep factor to fast CI assembly; * conclusions.

--- hw03-gc/src/main/java/ru/geracimov/otus/java/gc/langolier/helper/LangolierHelper.java
+++ hw03-gc/src/main/java/ru/geracimov/otus/java/gc/langolier/helper/LangolierHelper.java
@@ -1,6 +1,25 @@
 package ru.geracimov.otus.java.gc.langolier.helper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LangolierHelper {
+    private static final Logger logger = LoggerFactory.getLogger(LangolierHelper.class);
+    public static final String LANGOLIER_APPETITE = "LANGOLIER_APPETITE";
+    private static final int APPETITE_DEFAULT = 8;
+    private static int appetite;
+
+    static {
+
+        try {
+            final String envLangolierAppetite = System.getenv(LANGOLIER_APPETITE);
+            appetite = Integer.parseInt(envLangolierAppetite);
+        } catch (NumberFormatException nfe) {
+            appetite = APPETITE_DEFAULT;
+            logger.error("ENV {} is unset! Appetite set default value: {}", LANGOLIER_APPETITE, appetite, nfe);
+        }
+    }
+
 
     public void sleep(long millis) {
         try {
@@ -16,7 +35,7 @@ public class LangolierHelper {
 
     public long calcDuration(long memoryAvailable) {
         final long mb = mb(memoryAvailable);
-        return 100_000_000 / (8 * 128 * mb);
+        return 100_000_000 / (appetite * 128 * mb);
     }
 
 }
