b95228459e64380e7fc714ae7084e1c1136714cd
geracimov <geracimov@gmail.com>
2020-04-09 23:00:04 +0300

    hw11-cache added tests

--- hw11-cache/src/test/java/ru/geracimov/otus/java/cache/MyCacheTest.java
+++ hw11-cache/src/test/java/ru/geracimov/otus/java/cache/MyCacheTest.java
@@ -1,37 +1,49 @@
 package ru.geracimov.otus.java.cache;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SuppressWarnings({"Convert2Diamond", "Convert2Lambda"})
 public class MyCacheTest {
-    private static CacheManager cacheManager;
     private HwCache<String, String> cache;
-
-    @BeforeAll
-    static void beforeAll() {
-        cacheManager = new CacheManager();
-    }
 
     @BeforeEach
     public void setUp() {
+        CacheManager cacheManager = new CacheManager();
         cache = cacheManager.createCache("temp", String.class, String.class);
     }
 
     @Test
-    public void put() {
+    public void putTest() {
         cache.put("key1", "value1");
-        assertThat(cache.get("key1")).isNotNull();
+        assertThat(cache.get("key1")).isNotNull().isEqualTo("value1");
+        cache.put("key1", "value2");
+        assertThat(cache.get("key1")).isNotNull().isEqualTo("value2");
+        cache.put("key1", "value3");
+        assertThat(cache.get("key1")).isNotNull().isEqualTo("value3");
+        cache.put("key1", "value4");
+        assertThat(cache.get("key1")).isNotNull().isEqualTo("value4");
     }
 
     @Test
-    public void remove() {
+    public void removeTest() {
+        assertThat(cache.get("key1")).isNull();
+        cache.remove("key1");
+        assertThat(cache.get("key1")).isNull();
+        cache.put("key1", "value1");
+        cache.remove("key1");
+        cache.remove("key1");
+        cache.remove("key1");
+        assertThat(cache.get("key1")).isNull();
     }
 
     @Test
-    public void get() {
+    public void getTest() {
+        assertThat(cache.get("absentKey")).isNull();
+        cache.put("key1", "value4");
+        assertThat(cache.get("key1")).isEqualTo("value4");
     }
 
     @Test
