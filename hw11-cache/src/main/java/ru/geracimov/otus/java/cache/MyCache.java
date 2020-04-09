646db838b034859dfdb92d5af20bd8769c85c433
geracimov <geracimov@gmail.com>
2020-04-09 23:01:21 +0300

    hw11-cache added implementation

--- hw11-cache/src/main/java/ru/geracimov/otus/java/cache/MyCache.java
+++ hw11-cache/src/main/java/ru/geracimov/otus/java/cache/MyCache.java
@@ -4,34 +4,45 @@ import java.util.WeakHashMap;
 
 public class MyCache<K, V> implements HwCache<K, V> {
     private final WeakHashMap<K, V> cache;
+    private final WeakHashMap<HwListener<K, V>, Void> listeners;
 
     MyCache(WeakHashMap<K, V> cache) {
         this.cache = cache;
+        this.listeners = new WeakHashMap<>();
     }
 
     @Override
     public void put(K key, V value) {
-
+        cache.put(key, value);
+        notifyAllListeners(key, value, "PUT");
     }
 
     @Override
     public void remove(K key) {
-
+        V value;
+        if ((value = cache.get(key)) != null) {
+            cache.remove(key);
+            notifyAllListeners(key, value, "REMOVE");
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return cache.get(key);
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-
+        listeners.put(listener, null);
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
+        listeners.remove(listener);
+    }
 
+    private void notifyAllListeners(K key, V value, String action) {
+        listeners.keySet().forEach(l -> l.notify(key, value, action));
     }
 
 }
