6297861ef1901b72127af4a54343101d29785c4e
geracimov <geracimov@gmail.com>
2020-04-26 21:58:52 +0300

    hw13-ioc fixed review comment

--- hw13-ioc/src/main/java/ru/geracimov/otus/java/web/server/component/EmbeddedRedisServer.java
+++ hw13-ioc/src/main/java/ru/geracimov/otus/java/web/server/component/EmbeddedRedisServer.java
@@ -1,31 +1,31 @@
 package ru.geracimov.otus.java.web.server.component;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Component;
 import redis.embedded.RedisServer;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.io.IOException;
 
 @Slf4j
 @Component
 @Conditional(EmbeddedRedisCondition.class)
-public class EmbeddedRedisServer implements InitializingBean, DisposableBean {
+public class EmbeddedRedisServer {
     private final RedisServer redisServer;
 
     public EmbeddedRedisServer() throws IOException {
         this.redisServer = new RedisServer();
     }
 
-    @Override
+    @PostConstruct
     public void afterPropertiesSet() {
         this.redisServer.start();
         log.info("Embedded Redis started");
     }
 
-    @Override
+    @PreDestroy
     public void destroy() {
         this.redisServer.stop();
     }
