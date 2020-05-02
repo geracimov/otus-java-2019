7f36cfbfa208d0995c928aa5495c4eb6476d4933
geracimov <geracimov@gmail.com>
2020-05-02 18:59:42 +0300

    hw14-jmm fixed comment review

--- hw14-jmm/src/main/java/ru/geracimov/otus/java/thread/NumberSequence.java
+++ hw14-jmm/src/main/java/ru/geracimov/otus/java/thread/NumberSequence.java
@@ -7,8 +7,22 @@ public class NumberSequence {
     public static void main(String[] args) throws InterruptedException {
         final CountDownLatch latch = new CountDownLatch(1);
         NumberLogger numberLogger = new NumberLogger(1, 10);
-        Thread t1 = new Thread(() -> numberLogger.printNumber(200), "firstThread");
-        Thread t2 = new Thread(() -> numberLogger.printNumber(1000), "secondThread");
+        Thread t1 = new Thread(() -> {
+            try {
+                latch.await();
+                numberLogger.printNumber(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "firstThread");
+        Thread t2 = new Thread(() -> {
+            try {
+                latch.await();
+                numberLogger.printNumber(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "secondThread");
 
         t1.start();
         t2.start();
