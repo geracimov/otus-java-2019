3d95726b24d3357d015bbd8bc4dd165b8fdcd263
geracimov <geracimov@gmail.com>
2020-05-05 21:07:06 +0300

    hw16-multiprocess initial commit

---
+++ hw16-multiprocess/src/main/java/ru/geracimov/otus/java/multiprocess/MultiprocessApplication.java
@@ -0,0 +1,13 @@
+package ru.geracimov.otus.java.multiprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiprocessApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( MultiprocessApplication.class, args );
+    }
+
+}
